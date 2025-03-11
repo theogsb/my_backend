f30d5f5e814da10991cf6cb13976cb150591da8f
Marcos <maqvn@cin.ufpe.br>
2025-03-10 23:49:08 -0300

    Delete theo directory

--- theo/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { addTemplate, addSchedule } from '../controllers/userControllers.js'; 
-
-const router = express.Router();
-
-router.post('/:email/template', addTemplate);
-router.post('/:email/schedule', addSchedule);
-
-export default router
